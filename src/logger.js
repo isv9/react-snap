fda897df7aedc1db4fa7a0182751a5ffe684c287
isv9 <i.sarychev@relines.ru>
2021-06-02 14:27:14 +0300

    refactoring

--- src/logger.js
+++ src/logger.js
@@ -11,10 +11,15 @@ function createLogger(userOptions) {
       }
     },
     log(message) {
-      console.log(this.timestamp + message);
+      console.log(`${this.timestamp}: ${message}`);
     },
     get timestamp() {
-      return new Date().toLocaleString();
+      const date = new Date();
+      const day = date.getDate().toString().padStart(2, "0");
+      const month = (date.getMonth() + 1).toString().padStart(2, "0");
+      const minutes = date.getMinutes().toString().padStart(2, "0");
+      const hours = date.getHours().toString().padStart(2, "0");
+      return `[${day}-${month}-${date.getFullYear()} ${hours}:${minutes}]`;
     },
   };
 }
